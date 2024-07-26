2d3deeb38497462672aee998638f5baf3ee94421
Tanay Mehta <162467566+mehtatanay15@users.noreply.github.com>
2024-07-26 12:20:01 +0530

    Update uni_task2.js
    added the route to get api by id

--- uni_task2.js
+++ uni_task2.js
@@ -2,32 +2,40 @@ import express from 'express';
 import fetch from 'node-fetch';
 const app = express();
 
-// Route to get all characters
+// get all characters
 app.get('/characters', async (req, res) => {
     const reply = await fetch('https://hp-api.onrender.com/api/characters');
     const character = await reply.json();
     res.status(200).json(character);
 });
 
-// Route to get all spells
+// get all spells
 app.get('/spells', async (req, res) => {
     const reply = await fetch('https://hp-api.onrender.com/api/spells'); 
     const spell = await reply.json();
     res.status(200).json(spell);
 });
 
-// Route to get all Hogwarts staff
+// get all hogwarts staff
 app.get('/characters/staff', async (req, res) => {
     const reply = await fetch('https://hp-api.onrender.com/api/characters/staff'); 
     const staff = await reply.json();
     res.status(200).json(staff);
 });
 
-// Route to get all Hogwarts students
+// get all hogwarts students
 app.get('/characters/students', async (req, res) => {
     const reply = await fetch('https://hp-api.onrender.com/api/characters/students'); 
     const student = await reply.json();
     res.status(200).json(student);
+});
+
+//get character by id
+app.get('/characters/:id', async (req, res) => {
+    const reply= await fetch('https://hp-api.onrender.com/api/characters');
+    const characters = await reply.json();
+    const specificcharacter = await characters.find(character => character.id === req.params.id);
+    res.status(200).json(specificcharacter);
 });
 
 // Start the server
