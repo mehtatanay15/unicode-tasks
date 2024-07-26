cdf6b79d7effa8cded683bffbcb758942061a31b
Tanay Mehta <162467566+mehtatanay15@users.noreply.github.com>
2024-07-26 12:58:54 +0530

    post and get route done
    have written the code for post(add a student) and get(display all the students) still left to randomly assign the house to the student

---
+++ uni_task3.js
@@ -0,0 +1,49 @@
+import express from 'express';
+const app = express()
+import mongoose, { Schema } from 'mongoose';
+
+mongoose.connect('mongodb+srv://mehtatanay04:tanaymehta15@synapse.wwdu1cz.mongodb.net/unicode?retryWrites=true&w=majority&appName=Synapse')
+.then(() => {
+    console.log("Connected to MongoDB");
+})
+.catch((err) => {
+    console.error("Failed to connect to MongoDB", err);
+});
+
+app.use(express.json());
+
+const schema =mongoose.Schema({
+    id: String,
+    name: String,
+    gender: String,
+    house: String,
+    wizard: Boolean, 
+})
+
+const Student = mongoose.model('Student',schema)
+
+app.post('/addstudent', async(req,res) => {
+    try{
+        const student = await Student.create(req.body);
+        res.status(200).json(student);
+    }
+    catch(error)
+    {
+        res.status(500).json({message : error.message });
+    }
+})
+
+app.get('/students', async(req,res) => {
+    try{
+        const reply = await Student.find();
+        res.status(200).json(reply);
+    }
+    catch(error)
+    {
+        res.status(500).json({message :error.message})
+    }
+})
+
+app.listen(3000, function () {
+    console.log('listening on 3000')
+  })
